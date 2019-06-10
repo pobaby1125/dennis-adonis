662d0e6bd1f73fc69a186e948ca5d55ed32868c5
pobaby1125 <pobaby1125@qq.com>
2019-06-11 00:58:06 +0800

    '路由相关'

---
+++ app/Controllers/Http/PostController.js
@@ -0,0 +1,33 @@
+'use strict'
+
+class PostController {
+    index(){
+        return 'List of posts from controller'
+    }
+
+    store(){
+        return `Post has been created.`
+    }
+
+    show({params}){
+        return `You're watching post ${params.id}.`
+    }
+
+    update({params}){
+        return `Post ${params.id} has been updated.`
+    }
+
+    destroy({params}){
+        return `Post ${params.id} has been removed.`
+    }
+
+    create(){
+        return `Create post.`
+    }
+
+    edit({params}){
+        return `Editing post ${params.id}.`
+    }
+}
+
+module.exports = PostController
--- start/routes.js
+++ start/routes.js
@@ -19,8 +19,40 @@ const Route = use('Route')
 
 Route.on('/').render('welcome')
 
-Route.get('/hello', 'HelloController.render')
+//Route.get('/hello', 'HelloController.render')
 
-Route.get('/posts', async()=>{
-    return await Database.table('posts').select('*')
+// 读取数据库
+// Route.get('/posts', async()=>{
+//     return await Database.table('posts').select('*')
+// })
+
+/*
+Route.get('/posts', 'PostController.index')
+Route.post('/posts', 'PostController.store')
+Route.get('/posts/create', 'PostController.create')
+Route.get('/posts/:id', 'PostController.show')
+Route.patch('/posts/:id', 'PostController.update')
+Route.delete('/posts/:id', 'PostController.destroy')
+Route.get('/posts/:id/edit', 'PostController.edit')
+*/
+
+//以上可以浓缩为下面写法
+Route.resource('/posts', 'PostController')
+//.except(['index'])
+//.only(['index','show'])
+//.apiOnly()
+
+//命名路由
+Route.get('/hello', 'HelloController.render')
+     .as('users.index')
+
+// 路由群组     
+Route.group( ()=>{
+     Route.get('users', ()=>'Manage users')
+     Route.get('posts', ()=>'Manage posts')
 })
+.prefix('admin')
+
+
+// 单页面应用的路由
+Route.any('*',({view})=>view.render('welcome'))
