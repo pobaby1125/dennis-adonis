f882b44f0df256dca2545cb3b1761cfdb97d19ff
pobaby1125 <pobaby1125@qq.com>
2019-06-16 21:02:19 +0800

    'ICU设置相关信息'

--- app/Controllers/Http/AntlDemoController.js
+++ app/Controllers/Http/AntlDemoController.js
@@ -1,12 +1,33 @@
 'use strict'
 
+const Formats = use('Antl/Formats')
+
+Formats.add('longWeekDay', {
+    weekday: 'long'
+})
+
 class AntlDemoController {
     async demo({ view, antl, locale }) {
 
         return view.render( 'demo.antl', {
             greeting: antl.formatMessage('demo.greeting'),
             locale,
-            message: 'hello'
+            message: antl.formatMessage('demo.message', {
+                gender:'male'
+            }),
+            candy: antl.formatMessage('demo.candy', {
+                count: 0
+            }),
+            /*
+            message: antl.formatNumber(30, {
+                style: 'currency',
+                currency: 'cny'
+            })
+            */
+
+            day: antl.formatMessage('demo.day', { today:new Date() }, [
+                Formats.pass('longWeekDay', 'date')
+            ])
         })
     }
 }
